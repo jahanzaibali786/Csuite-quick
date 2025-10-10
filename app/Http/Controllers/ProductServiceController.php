43320bf3221749e477d345551d18bab95bd1f550
Muhammad Abdullah Ali <118581404+AbdullahAli420@users.noreply.github.com>
2025-10-10 12:45:04 +0500

    Reports with query issues solved. 08/10/2025

--- app/Http/Controllers/ProductServiceController.php
+++ app/Http/Controllers/ProductServiceController.php
@@ -136,10 +136,16 @@ class ProductServiceController extends Controller
             'selectedType' => $request->get('type', ''),
         ];
 
-        return $dataTable->render(
-            'productservice.incomeByCustomerSummary',
-            compact('pageTitle', 'category', 'types', 'user', 'filter')
-        );
+        // return $dataTable->render(
+        //     'productservice.incomeByCustomerSummary',
+        //     compact('pageTitle', 'category', 'types', 'user', 'filter')
+        // );
+
+        return $dataTable->render('sync.simpletable.index', [ // ✅ keep same view, or create vendorbalance.index
+            'pageTitle' => $pageTitle,
+            'startDate' => $request->get('start_date', date('Y-01-01')),
+            'endDate' => $request->get('end_date', date('Y-m-d', strtotime('+1 day')))
+        ]);
     }
 
     public function incomeByCustomerSummaryTwo(
